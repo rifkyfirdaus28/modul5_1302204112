3602e03e12e7ff6a3a7858fdf12a7f81acecde78
Praktikan <PRAKTIKAN@0605-PC-21>
2023-03-20 07:08:06 +0700

    Add project files.

---
+++ modul5_1302204112/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace modul5_1302204112
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
