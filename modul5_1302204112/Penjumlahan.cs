29c6acab75bfc1368366b73b4eb7de909bb33e2a
rifkyfrds <100988194+rifkyfirdaus28@users.noreply.github.com>
2023-03-20 08:21:02 +0700

    Add files via upload
    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ modul5_1302204112/Penjumlahan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302204112
+{
+    internal class Penjumlahan
+    {
+        public static T jumlahTigaAngka<T>(T input1, T input2, T input3)
+        {
+            dynamic a = input1;
+            dynamic b = input2;
+            dynamic c = input3;
+
+            return a + b + c;  
+        }
+    }
+}
